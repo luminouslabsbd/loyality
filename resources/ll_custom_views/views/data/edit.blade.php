99cd46b147ae49f4805284742d07b9ff59087fce
Ahaduzzaman01 <abdulahad5161@gmail.com>
2023-11-09 10:14:22 +0600

    user edit page

--- resources/ll_custom_views/views/data/edit.blade.php
+++ resources/ll_custom_views/views/data/edit.blade.php
@@ -12,9 +12,38 @@ if($settings['overrideTitle']) {
 
 @section('content')
     <div class="w-full" @onclicktab="window.appSetImageUploadHeight()">
-        <div class="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg m-0 sm:m-4">
+        <div class="relative bg-white p-4 lg:p-6">
             <div
-                class="flex flex-col px-4 py-3 space-y-3 md:flex-row md:items-center md:justify-between md:space-y-0 md:space-x-4 border-b dark:border-gray-700">
+                class="mb-3">
+                <div class="w-full flex flex-row items-center justify-between">
+                    @if($settings['list'])
+                        <div class="mb-5">
+                            <a href="{{ route($settings['guard'].'.data.list', ['name' => $dataDefinition->name]) }}"
+                                class="ll-back-btn w-fit flex text-sm items-center justify-start">
+                                <x-ui.icon icon="left" class="mr-2" />
+                                {{ trans('common.back_to_list') }}
+                            </a>
+                        </div>
+                    @endif
+
+                    <div class="flex flex-row items-center justify-end">
+                        @if ($settings['view'])
+                            <a href="{{ route($settings['guard'].'.data.view', ['name' => $dataDefinition->name, 'id' => $form['data']->id]) }}"
+                                class="w-full flex items-center btn-sm text-sm mr-2 btn-primary ll-primary-btn">
+                                <x-ui.icon class="h-3.5 w-3.5 mr-2" icon="magnifying-glass" />
+                                {{ trans('common.view') }}
+                            </a>
+                        @endif
+                        @if ($settings['delete'])
+                            <button type="button" class="w-full flex items-center btn-sm btn-danger text-sm ll-danger-btn"
+                                @click="deleteItem('{{ $form['data']->id }}', '{{ $settings['subject_column'] ? str_replace("'", "\'", parse_attr($form['data']->{$settings['subject_column']})) : null }}')">
+                                <x-ui.icon class="h-3.5 w-3.5 mr-2" icon="trash" />
+                                {{ trans('common.delete') }}
+                            </button>
+                        @endif
+                    </div>
+                </div>
+
                 <div class="w-full flex items-center space-x-3">
                     @if($settings['list'])
                         <a href="{{ route($settings['guard'].'.data.list', ['name' => $dataDefinition->name]) }}">
@@ -38,116 +67,101 @@ if($settings['overrideTitle']) {
                         </div>
                     @endif
                 </div>
-                <div class="w-full flex flex-row items-center justify-end space-x-3">
-                    @if($settings['list'])
-                        <a href="{{ route($settings['guard'].'.data.list', ['name' => $dataDefinition->name]) }}"
-                            class="w-full md:w-auto flex btn-dark text-sm px-3 py-2 whitespace-nowrap text-ellipsis">
-                            <x-ui.icon icon="left" class="h-3.5 w-3.5 mr-2" />
-                            {{ trans('common.back_to_list') }}
-                        </a>
-                    @endif
-                    @if ($settings['view'])
-                        <a href="{{ route($settings['guard'].'.data.view', ['name' => $dataDefinition->name, 'id' => $form['data']->id]) }}"
-                            class="w-full md:w-auto flex btn-info text-sm px-3 py-2">
-                            <x-ui.icon class="h-3.5 w-3.5 mr-2" icon="magnifying-glass" />
-                            {{ trans('common.view') }}
-                        </a>
-                    @endif
-                    @if ($settings['delete'])
-                        <button type="button" class="w-full md:w-auto flex btn-danger text-sm px-3 py-2"
-                            @click="deleteItem('{{ $form['data']->id }}', '{{ $settings['subject_column'] ? str_replace("'", "\'", parse_attr($form['data']->{$settings['subject_column']})) : null }}')">
-                            <x-ui.icon class="h-3.5 w-3.5 mr-2" icon="trash" />
-                            {{ trans('common.delete') }}
-                        </button>
-                    @endif
-                </div>
             </div>
             @php
             $hasTabs = !empty($form['tabs']);
             @endphp
-            <div class="px-4 pb-4 @if($hasTabs) pt-1 sm:rounded-b-lg @endif">
+            <div class="@if($hasTabs) pt-1 @endif">
                 <x-forms.messages class="mt-4" />
                 <x-forms.form-open
                     :novalidate="$hasTabs"
                     action="{{ route($settings['guard'].'.data.edit.post', ['name' => $dataDefinition->name, 'id' => $form['data']->id]) }}"
-                    enctype="multipart/form-data" id="formDataDefinition" method="POST" class="space-y-4 md:space-y-6" />
+                    enctype="multipart/form-data" id="formDataDefinition" method="POST" class="ll-user-add-form space-y-4 md:space-y-6" />
                     @if ($form['columns'])
-                    @if($hasTabs)
-                        <x-ui.tabs :tabs="array_values($form['tabs'])" active-tab="1" class="space-y-4 md:space-y-6 py-6">
-                        @php
-                        $previousTab = null;
-                        @endphp
-                        @foreach ($form['columns'] as $column)
-                            @if (!$column['hidden'])
-                                @if($column['tab'] && $column['tab'] !== $previousTab)
-                                    @if($previousTab !== null)
-                                        </x-slot>
-                                    @endif
-                                    <x-slot :name="$column['tab']">
-                                @endif
-
-                                @if($column['container_start::insert'])
-                                    <div class="{{ $column['container_start::insert'] }}">
-                                @endif
-                                @if($column['classes::insert'])
-                                    <div class="{{ $column['classes::insert'] }}">
-                                @endif
-                                @include('data.form', compact('form', 'column'))
-                                @if($column['classes::insert'])
-                                    </div>
-                                @endif
-                                @if($column['container_end::insert'])
-                                    </div>
-                                @endif
+                        <div class="ll-user-add-form-inputs grid grid-cols-7 gap-x-5">
+                            @if($hasTabs)
+                                <x-ui.tabs :tabs="array_values($form['tabs'])" active-tab="1" class="space-y-4 md:space-y-6 py-6">
                                 @php
-                                $previousTab = $column['tab'];
+                                $previousTab = null;
                                 @endphp
+                                @foreach ($form['columns'] as $column)
+                                    @if (!$column['hidden'])
+                                        @if($column['tab'] && $column['tab'] !== $previousTab)
+                                            @if($previousTab !== null)
+                                                </x-slot>
+                                            @endif
+                                            <x-slot :name="$column['tab']">
+                                        @endif
+
+                                        @if($column['container_start::insert'])
+                                            <div class="{{ $column['container_start::insert'] }}">
+                                        @endif
+                                        @if($column['classes::insert'])
+                                            <div class="{{ $column['classes::insert'] }}">
+                                        @endif
+                                        @include('data.form', compact('form', 'column'))
+                                        @if($column['classes::insert'])
+                                            </div>
+                                        @endif
+                                        @if($column['container_end::insert'])
+                                            </div>
+                                        @endif
+                                        @php
+                                        $previousTab = $column['tab'];
+                                        @endphp
+                                    @endif
+                                @endforeach
+                                    </x-slot>
+                                </x-ui.tabs>
+                            @else
+                                @foreach ($form['columns'] as $column)
+                                    @if (!$column['hidden'])
+                                        @if($column['container_start::insert'])
+                                            <div class="{{ $column['container_start::insert'] }}">
+                                        @endif
+                                        @if($column['classes::insert'])
+                                            <div class="{{ $column['classes::insert'] }}">
+                                        @endif
+                                        @include('data.form', compact('form', 'column'))
+                                        @if($column['classes::insert'])
+                                            </div>
+                                        @endif
+                                        @if($column['container_end::insert'])
+                                            </div>
+                                        @endif
+                                    @endif
+                                @endforeach
                             @endif
-                        @endforeach
-                            </x-slot>
-                        </x-ui.tabs>
-                    @else
-                        @foreach ($form['columns'] as $column)
-                            @if (!$column['hidden'])
-                                @if($column['container_start::insert'])
-                                    <div class="{{ $column['container_start::insert'] }}">
-                                @endif
-                                @if($column['classes::insert'])
-                                    <div class="{{ $column['classes::insert'] }}">
-                                @endif
-                                @include('data.form', compact('form', 'column'))
-                                @if($column['classes::insert'])
-                                    </div>
-                                @endif
-                                @if($column['container_end::insert'])
-                                    </div>
-                                @endif
+                            
+                            @if ($settings['editRequiresPassword'])
+                                <div class="p-6 text-sm rounded-lg bg-gray-50 dark:bg-gray-900 border dark:border-gray-700">
+                                    <x-forms.input 
+                                        value=""
+                                        class-label="mb-4"
+                                        type="password"
+                                        name="current_password_required_to_save_changes"
+                                        icon="key"
+                                        :label="trans('common.current_password_to_save_changes')"
+                                        :placeholder="trans('common.current_password')"
+                                        :required="true"
+                                    />
+                                </div>
                             @endif
-                        @endforeach
-                    @endif
-                    @if ($settings['editRequiresPassword'])
-                        <div class="p-6 text-sm rounded-lg bg-gray-50 dark:bg-gray-900 border dark:border-gray-700">
-                            <x-forms.input 
-                                value=""
-                                class-label="mb-4"
-                                type="password"
-                                name="current_password_required_to_save_changes"
-                                icon="key"
-                                :label="trans('common.current_password_to_save_changes')"
-                                :placeholder="trans('common.current_password')"
-                                :required="true"
-                            />
+                        </div>
+                        <div class="grid grid-cols-7 ll-user-add-form-footer gap-x-5">
+                            <div class="ll-user-form-empty"></div>
+                            @if($settings['list'])
+                                <div class="col-span-3">
+                                    <a href="{{ route($settings['guard'].'.data.list', ['name' => $dataDefinition->name]) }}"
+                                    class="btn w-full ll-btn-white">{{ trans('common.cancel') }}</a>
+                                </div>
+                            @endif
+                            <div class="col-span-3">
+                                <button type="submit" class="w-full btn-primary ll-primary-btn" onmouseover="this.style.background='blue';" onmouseout="this.style.background='#2963FF';">{{ trans('common.save_changes') }}<span
+                                class="form-dirty hidden">&nbsp;*</span></button>
+                            </div>
                         </div>
                     @endif
-                    <div class="flex flex-col items-center justify-evenly space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
-                        <button type="submit" class="w-full rounded-lg btn-lg" style="background: #2963FF; color: white;" onmouseover="this.style.background='blue';" onmouseout="this.style.background='#2963FF';">{{ trans('common.save_changes') }}<span
-                                class="form-dirty hidden">&nbsp;*</span></button>
-                        @if($settings['list'])
-                            <a href="{{ route($settings['guard'].'.data.list', ['name' => $dataDefinition->name]) }}"
-                            class="btn btn-lg w-full">{{ trans('common.cancel') }}</a>
-                        @endif
-                    </div>
-                @endif
                 <x-forms.form-close />
                 @if (session('current_tab_index'))
                     <script>
@@ -187,5 +201,23 @@ if($settings['overrideTitle']) {
             }
         </script>
     @endif
+    
+    <script>
+        $(document).ready(function() {
+            const formInputs = $('.ll-user-add-form-inputs');
+            const childDivs = formInputs.children('div');
+            const divCount = childDivs.length;
 
+            // Select the first div inside the form and wrap it with a new div
+            childDivs.eq(0).wrap('<div class="ll-upload-img"></div>');
+
+            // Calculate the number of divs for the second and third divs
+            const secondDivCount = Math.floor((divCount - 1) / 2);
+            const thirdDivCount = divCount - 1 - secondDivCount;
+
+            // Select the remaining div elements and wrap them in two separate divs
+            childDivs.slice(1, secondDivCount + 1).wrapAll('<div class="col-span-3"></div>');
+            childDivs.slice(secondDivCount + 1).wrapAll('<div class="col-span-3"></div>');
+        });
+    </script>
 @stop
