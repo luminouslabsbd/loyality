999ff76b15b2071e67791de8d8fa91d7b27f3a48
Ahaduzzaman01 <abdulahad5161@gmail.com>
2023-11-18 23:22:44 +0600

    partner analytics page

--- resources/ll_views/partner/analytics/analytics.blade.php
+++ resources/ll_views/partner/analytics/analytics.blade.php
@@ -30,7 +30,7 @@
                 </div>
                 <div>
                     <div class="flex items-center pl-4">
-                        <input id="active_only" type="checkbox" value="true" @if($active_only == 'true') checked @endif name="active_only" class="w-4 h-4 text-primary-600 bg-gray-100 border-gray-300 rounded focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        <input id="active_only" type="checkbox" value="true" @if($active_only == 'true') checked @endif name="active_only" class="w-4 h-4 bg-gray-100 border-gray-300 rounded dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600">
                         <label for="active_only" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ trans('common.only_show_active_cards') }}</label>
                     </div>
                 </div>
